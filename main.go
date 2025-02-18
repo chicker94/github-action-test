1cc62b3c32e232adf6fa377a3ccdb343ce4000c9
key <chicker94@gmail.com>
2025-02-18 16:23:51 +0900

    dev 파일 추가

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 // 환경 설정
-const ENV = "prod" // "prod"로 변경 가능
+const ENV = "dev" // "prod"로 변경 가능
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Rancher CI/CD Test (%s)</h1>", ENV)
